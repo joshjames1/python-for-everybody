58f20fd1eb163a60bd851d3e8cf594a329aa0559
Josh James <josh.james@onshoretechnology.local>
2019-05-18 15:53:49 -0500

    file upload

---
+++ 4.1.py
@@ -0,0 +1,15 @@
+"""
+Exercise 4.1: Run the program on your system and see what numbers you get. 
+Run the program more than once and see what numbers you get.
+Python for Everybody: Exploring Data Using Python 3
+by Charles R. Severance
+Solution by Jamison Lahman, May 28, 2017
+"""
+
+import random
+
+for i in range(10):
+	x = random.random()
+	print(x)
+	
+"""
