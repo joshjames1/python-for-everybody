fffb7f58a939bbec42f20c20e8bcae253611a041
Josh James <josh.james@onshoretechnology.local>
2019-05-23 23:09:57 -0500

    file upload

--- example.py
+++ example.py
@@ -1,20 +1,7 @@
-def computepay(h,r):
-	if h<=40:
-		result = h*r
-	else:
-   	    o = h-40    	
-	result = o*(r*1.50) + 40*r
-	return result
+fname = input("Enter file name: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
 
-hours = input("enter hours:")
-rate = input("enter rate:")
-#hours = input("How many hours worked?")
-#rate = input("What is the pay rate?")
-try:
-    hours = float(hours)
-    rate = float(rate)
-except:
-    print ("please enter a valid value.")
-    quit()
-p = computepay(hours, rate)
-print (p)
+count = 0
+fh = open(fname)
+for line in fh:
+	print(line)
--- example2.py
+++ example2.py
@@ -1,3 +1,2 @@
-data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
-pos = data.find('.')
-print(data[pos:pos+3])
+stuff = dict()
+print(stuff.get('candy',-1))
